2872aa5ae9335119627199fae095fa794a6f783e
desterousDangi <inboxpardeepdangi@gmail.com>
2022-07-15 21:42:27 +0530

    First stand alone application using springboot

---
+++ First_project_for_SpringBoot/src/main/java/com/spring/first/AppConfiguration.java
@@ -0,0 +1,12 @@
+package com.spring.first;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AppConfiguration {
+	@RequestMapping("/hello")
+	public String hello() {
+		return "Hello World";
+	}
+}
---
+++ First_project_for_SpringBoot/src/main/java/com/spring/first/FirstProjectForSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.spring.first;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstProjectForSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstProjectForSpringBootApplication.class, args);
+	}
+
+}
